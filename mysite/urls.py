396ce1ecc3646aa8567258047ae6260d4bfb9c46
imyaxir <imyaxir@users.noreply.github.com>
2019-04-22 17:30:46 +0500

    new

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,13 +5,13 @@ from mysite.views import *
 
 urlpatterns = [
     path('', index),
-    path('/addDepartment', addDepartment ,name='adddept'),
-    path('/DeleteDepartment', DeleteDepartment,name='deletedept'),
-    path('/ViewDepartment', ViewDepartment,name='viewdept'),
-    path('/addDesignation', addDesignation,name='adddes'),
-    path('/RemoveDesignation', RemoveDesignation,name='deletedes'),
-    path('/ViewDesignation',ViewDesignation,name='viewdes'),
-    path('/addOffice', addOffice,name='addo'),
-    path('/viewOffice', viewOffice,name='viewo'),
-    path('/RemoveOffice', RemoveOffice,name='deleteo'),
+    path('addDepartment', addDepartment ,name='adddept'),
+    path('DeleteDepartment', DeleteDepartment,name='deletedept'),
+    path('ViewDepartment', ViewDepartment,name='viewdept'),
+    path('addDesignation', addDesignation,name='adddes'),
+    path('RemoveDesignation', RemoveDesignation,name='deletedes'),
+    path('ViewDesignation',ViewDesignation,name='viewdes'),
+    path('addOffice', addOffice,name='addo'),
+    path('viewOffice', viewOffice,name='viewo'),
+    path('RemoveOffice', RemoveOffice,name='deleteo'),
 ]
